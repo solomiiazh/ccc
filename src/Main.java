d5987ad6e812af673839bda9f3c2d52152aa581c
Solomiia Zhuromska <solomiiasolia@gmail.com>
2020-10-21 14:36:47 +0300

    added new double array

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello Git");
-        int[] arr = {12,15,16,18};
-        for (int x: arr   ) {
+
+        double[] arr = {12.12,15.16,16.12,18};
+        for (double x: arr   ) {
             System.out.print(x +  " ");
         }
     }
